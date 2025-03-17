f6833a25f8a2a8c183a45b65d3871e762a435ea5
luizoctaviopks <luiiz.octaviio.bdc@gmail.com>
2025-03-17 11:35:12 -0300

    prova

--- aplicativos/AppPilotos.java
+++ aplicativos/AppPilotos.java
@@ -23,7 +23,7 @@ public class AppPilotos {
                 System.out.println("2 - Listar pilotos cadastrados");
                 System.out.println("3 - Localizar piloto pelo CPF");
                 System.out.println("4 - Cadastrar aeronave");
-                System.out.println("0 - Sair");
+                System.out.println("0 - Sair");                
                 System.out.print("Opção: ");
     
                 opcao = in.nextInt();
